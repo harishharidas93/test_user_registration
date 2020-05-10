9940eb35d0480241e0dedce670a003d42daf30ca
harish <harish@softincubator.com>
2020-05-10 13:05:15 +0530

    Completed all except redis part

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,23 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://test_user:test_pass@cluster0-n9qah.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true});
-
+const redis = require("redis");
+const jwt = require('jsonwebtoken');
+const jwt_secret = "jwttokenaccubits";
+const jwt_expiration = 60 * 10;
+const jwt_refresh_expiration = 60 * 60 * 24 * 30;
+var rediscl = redis.createClient();
+rediscl.on("connect", function () {
+    console.log("Redis plugged in.");
+});
 
 MongoClient.connect(uri, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const userCollection = client.db("test").collection("users");
+
     router.post('/register_user', (req, res) => {
-      userCollection.insertOne(req.query)
+      userCollection.insertOne({...req.query,tokens: []})
         .then(result => {
             res.send({
                 status : 'success',
@@ -25,11 +33,16 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
           })
     })
 
+    // To get all user details
     router.get('/get_user_details', (req, res) => {
-        userCollection.find().toArray()
+        userCollection.find().project({name: 1, email:1, _id:0}).toArray()
           .then(results => {
-            // console.log(results)
-            res.send({status:'success',data:results})
+            if(results.length == 0){
+              res.send({
+                status: 'error',
+                message: 'No user added'
+              });
+            } else res.send({status:'success',data:results})
           })
           .catch(error => {
             console.error(error);
@@ -38,25 +51,76 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
                 message: error
             });
           })
-      })
+    })
+
+    // To get login details of a specific user
+    router.get('/get_user_login_details', (req, res) => {
+        userCollection.find(req.query).project({tokens: 1,_id:0}).toArray()
+          .then(results => {
+            if(results.length == 0){
+              res.send({
+                status: 'error',
+                message: 'No login detected'
+              });
+            } else res.send({status:'success',data:results})
+          })
+          .catch(error => {
+            console.error(error);
+            res.send({
+                status : 'error',
+                message: error
+            });
+          })
+    })
       
-      router.post('/login', (req, res) => {
-        userCollection.find(req.query).toArray()
-          .then(results => {
-            // Given length greater than 0 since currently while testing there are chances that same username may exist more than once
-            if(results && results.length > 0) {
-                console.log(results)
+    router.post('/login', (req, res) => {
+      userCollection.find(req.query).toArray()
+        .then(results => {
+          if(results && results.length == 1) {
+              let user_name = req.query.name;
+              // let refresh_token = generate_refresh_token(64);
+              // let refresh_token_maxage = new Date() + jwt_refresh_expiration;
+              // Generate new access token
+              let token = jwt.sign({ uid: user_name }, jwt_secret, {
+                  expiresIn: jwt_expiration
+              });
+              // And store the user in Redis under key 2212
+              // redis.set(user_id, JSON.stringify({
+              //   refresh_token: refresh_token,
+              //   expires: refresh_token_maxage
+              //   }),
+              //   redis.print
+              // );
+              // rediscl.set(user_name, JSON.stringify({
+              //   expires: refresh_token_maxage
+              //   }),
+              //   rediscl.print
+              // );
+              // let user = results[0];
+              userCollection.findOneAndUpdate({ name: user_name },
+              {
+                $push: {
+                  tokens: {
+                    token: token,
+                    time: new Date()
+                  }
+                }
+              }, {upsert : true})
+              .then(result => {
                 res.send({status:'success'})
-            } else { res.send({status:'no_user_found'}) }
-          })
-          .catch(error => {
-            console.error(error);
-            res.send({
-                status : 'error',
-                message: error
-            });
-          })
-      })
+              })
+              .catch(error => console.error(error))
+              
+          } else { res.send({status:'no_user_found'}) }
+        })
+        .catch(error => {
+          console.error(error);
+          res.send({
+              status : 'error',
+              message: error
+          });
+        })
+    })
   })
   .catch(error => {
     console.error(error);
